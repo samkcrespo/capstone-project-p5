3929efbb1f2fcef4b750578089dfcbb36932c401
Samantha Crespo <samkcrespo@gmail.com>
2022-06-30 12:03:48 -0700

    setting up heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding users 🌱'
+
+user1 = User.create!(first_name: 'John', last_name: 'Doe', email: 'john@example.com', password: 'password')
+user2 = User.create!(first_name: 'Sarah', last_name: 'Smith', email: 'sarah@example.com', password: 'password')
+user3 = User.create!(first_name: 'Kelly', last_name: 'Doe', email: 'kelly@example.com', password: 'password')
+user4 = User.create!(first_name: 'Sam', last_name: 'Crespo', email: 'sam@example.com', password: 'password')
+
+puts 'Users seeded successfully! 👤✔️'
